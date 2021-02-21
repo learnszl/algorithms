8749f55de3fae5f6ae5f5acf34593398288e114e
learnszl <ahsongzhili@163.com>
2021-02-21 11:02:23 +0800

    开场白

---
+++ basic/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author szl
+ * @Date 2021/2/21 10:59
+ * @Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("GitHub");
+    }
+}
